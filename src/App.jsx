a72b60a34796d23ab8dcdf95be5d1c49caaf7f53
Eslavita <santiagoeslava906@gmail.com>
2026-03-26 18:38:24 -0500

    Configure project for GitHub Pages deployment (HashRouter and base path)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Empresa from './pages/Empresa/Empresa';
 import Navbar from './components/common/Navbar';
